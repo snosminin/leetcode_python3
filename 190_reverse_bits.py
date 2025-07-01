f6f7d22ee3c77f95150a4727db3926454a00656c
Sergei <>
2025-07-01 20:43:59 +0600

    Add 190 reverse bits

---
+++ 190_reverse_bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+
+        for i in range(32):
+            result |= (n & 1) << (31 - i)
+            n >>= 1
+
+        return result
+
+print(Solution().reverseBits(n = 11))
